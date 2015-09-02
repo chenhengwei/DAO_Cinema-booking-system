6a7e7252072ed0f915ed39899b5d1a6dd2db17aa
chenhengwei <onlylimit@gmail.com>
2015-09-02 17:35:15 +0800

    V0902_1800
    更新 movie 跟 session

--- src/edu/pccu/Movie/AllJoinDAO.java
+++ src/edu/pccu/Movie/AllJoinDAO.java
@@ -9,4 +9,6 @@ public interface AllJoinDAO {
     public ArrayList<AllJoin> getOrderedTickets(String mail_account,String phone_password);
     //查詢已訂電影票(後台)
     public ArrayList<AllJoin> getOrderedTickets(String mail_account);
+    //取得使用者指定時間內的場次
+    public ArrayList<Session> getSession(int movie_no,String show_date);
 }
