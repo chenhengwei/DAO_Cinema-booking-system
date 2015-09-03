f1c212420ccf07d8af3cc7cda2b11f5ef7953c1a
chenhengwei <onlylimit@gmail.com>
2015-09-03 20:14:05 +0800

    V0903_2000
    Ticket DAO

--- src/T_find_sold_by_RS.java
+++ src/T_find_sold_by_RS.java
@@ -10,6 +10,9 @@ public class T_find_sold_by_RS {
         String s = saet_dao.find_Seat_by_RS(R, S);
         System.out.println(s);
 
+        
+        String[] arr_R = R.split(",");
+	String[] arr_S = S.split(",");
     }
 
 }
